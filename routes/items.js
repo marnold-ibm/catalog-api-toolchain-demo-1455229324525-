9059d0ecdf9f62a880b59a82e897073c47872482
marnold-ibm <marnold@us.ibm.com>
2016-02-11 18:52:56 -0500

    Update items.js

--- routes/items.js
+++ routes/items.js
@@ -12,7 +12,7 @@ var USE_FASTCACHE = false;
 exports.loadTest = function(req, res) {
 //   res.json({"success": 0, "fail": 0, "time": 0});
 
-    USE_FASTCACHE = true;
+    USE_FASTCACHE = false;
     var testCount = req.query.count;
     testCount = testCount ? parseInt(testCount) : 100;
 
